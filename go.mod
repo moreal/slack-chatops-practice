3aab3d2239830b72fe45913dac52f240ffd03e18
moreal <dev.moreal@gmail.com>
2020-03-10 02:43:35 +0900

    Implement simple dispatch request

--- go.mod
+++ go.mod
@@ -3,7 +3,13 @@ module github.com/moreal/slack-chatops-practice
 go 1.13
 
 require (
+	github.com/golang/protobuf v1.3.4 // indirect
+	github.com/google/go-github v17.0.0+incompatible
+	github.com/google/go-github/v29 v29.0.3
 	github.com/gorilla/websocket v1.4.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/slack-go/slack v0.6.3
+	golang.org/x/net v0.0.0-20200301022130-244492dfa37a // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+	google.golang.org/appengine v1.6.5 // indirect
 )
--- main.go
+++ main.go
@@ -1,15 +1,39 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"github.com/google/go-github/v29/github"
 	"github.com/slack-go/slack"
+	"golang.org/x/oauth2"
 	"log"
 	"net/http"
+	"os"
 )
 
+var client *github.Client
+var ctx = context.Background()
+
 func main() {
+	token := os.Getenv("GITHUB_AUTH_TOKEN")
+	initGithubClient(token)
+
+	bytes, _ := json.Marshal(struct { Id string } { Id: ""})
+	payload := json.RawMessage(bytes)
+	client.Repositories.Dispatch(ctx, "moreal", "slack-chatops-practice", github.DispatchRequestOptions{
+		EventType: "go",
+		ClientPayload: &payload,
+	})
+
 	http.HandleFunc("/commands/deploy", deployCommandHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func initGithubClient(token string) {
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
+	tc := oauth2.NewClient(ctx, ts)
+	client = github.NewClient(tc)
 }
 
 func deployCommandHandler(w http.ResponseWriter, r *http.Request) {
